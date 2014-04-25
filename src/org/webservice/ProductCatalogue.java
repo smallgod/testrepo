2bfa48287f82bd8b9477d75581d570427ba4ae37
smallg <smallgod.sg@gmail.com>
2014-04-25 19:24:21 +0300

    test commit

---
+++ src/org/webservice/ProductCatalogue.java
@@ -0,0 +1,23 @@
+package org.webservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class ProductCatalogue {
+
+	@WebMethod
+	public List<String>getProductCategories(){
+		
+		List<String> categories = new ArrayList<>();
+		categories.add("Books");
+		categories.add("Digital");
+		categories.add("Music");
+		categories.add("Movies");
+		
+		return categories;		
+	}
+}
